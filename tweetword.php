0690243436670c5c99b6e528422c04a8b7a49ffc
Graham Ballantyne <hello@grahamballantyne.com>
2010-12-30 12:01:40 -0800

    added tweetword.php to tweet current word

---
+++ tweetword.php
@@ -0,0 +1,11 @@
+<?
+
+require_once('safetweeter.php');
+$word = get_current_word(true);
+$today = date("F j");
+$staus = "The lunchtime safeword for $today is " . $word['word'] . " (via @" . $word['addedby'] . ")";
+send_message('public', array(
+	'status' => $status;
+));
+
+?>
